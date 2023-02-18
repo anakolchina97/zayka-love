c4d7f2a34ae00e9d0fab84a91af9d2d9e5429525
Annie <anniehartmann97@gmail.com>
2023-02-19 00:55:13 +0300

    Add scss

---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <header class="header">Шапка карточки</header>
+  </section>
+</template>
+<style lang="scss" scoped>
+.header {
+  height: 200px;
+  background: orange;
+  &:hover {
+    background: green;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <header class="header">Logotype Nav Menu</header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  height: 100px;
+  background: var(--color-primary);
+}
+</style>
